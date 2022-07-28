d02eb3d186a20787f48705e3cb73c21c85b67d7d
Costi <sova.victor98@gmail.com>
2022-07-28 12:46:42 +0300

    Exercitii cu colectii 2

---
+++ ExercitiiColectii2/Preturi.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercitiiColectii2
+{
+    internal class Preturi
+    {
+        private List <double> listaPreturi;
+        public Preturi()
+        {
+            this.listaPreturi = new List<double>();
+        }
+
+        public void adaugarePret(double pret)
+        {
+            listaPreturi.Add(pret);
+        }
+
+        public void afisareListaPreturi()
+        {
+            foreach(double element in listaPreturi)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public override string ToString()
+        {
+            string preturiElemente = "";
+            foreach(double element in listaPreturi)
+            {
+                preturiElemente = preturiElemente + " " + element.ToString();
+            }
+            return preturiElemente;
+        }
+
+        public void stergerePrimulElement()
+        {
+            listaPreturi.RemoveAt(0);
+        }
+
+        public void stergereUltimulElement()
+        {
+            listaPreturi.RemoveAt(listaPreturi.Count() - 1);
+        }
+
+        public void sortareListaDePreturiCrescator()
+        {
+            listaPreturi.Sort();
+        }
+
+        public void sortareListaDePreturiDescrescator()
+        {
+            listaPreturi.Sort();
+            listaPreturi.Reverse();
+        }
+
+        public double getElementAt(int pozitie)
+        {
+            return listaPreturi[pozitie];
+        }
+
+        public double getTotal()
+        {
+            return listaPreturi.Sum();
+        }
+
+        public int countPreturi()
+        {
+            return listaPreturi.Count();
+        }
+
+        public List<double> getListaPreturiAbove(double pretDeComparat)
+        {
+            List<double> lista2 = new List<double>();
+            foreach(double element in listaPreturi)
+            {
+                if(pretDeComparat<element)
+                {
+                    lista2.Add(element);
+                }
+            }
+            return lista2;
+        }
+
+        public List<double> getListaPreturiAbove2(double pretDeComparat)
+        {
+            List<double> lista2 = new List<double>();
+            lista2 = listaPreturi.FindAll(pret => pret > pretDeComparat);
+            return lista2;
+
+            // return listaPreturi.FindAll(pret => pret > pretDeComparat);
+        }
+
+        //O metoda care returneaza cate elemente sunt sub un pret dat
+        public int getNumberOfElementsBelow(double pretDeComparat2)
+        {
+            List<double> lista3 = new List<double>();
+            lista3 = listaPreturi.FindAll(pret => pret < pretDeComparat2);
+            return lista3.Count();
+        }
+
+        //O metoda care returneaza suma elementelor fara cel mai mic element
+        
+        public double sumWithoutMin()
+        {
+            double suma;
+            List<double> auxiliar = new List<double>(listaPreturi);
+            auxiliar.Sort();
+            auxiliar.RemoveAt(0);
+            return suma = auxiliar.Sum();
+        }
+
+        //O metoda care returneaza media celor mai mari trei elemente
+        public double mediaCelorMaiMariTreiElemente()
+        {
+            double media;
+            listaPreturi.Sort();
+            listaPreturi.RemoveRange(0, 3);
+            media = listaPreturi.Sum();
+            return media;
+        }
+
+        //To do
+        //O metoda care returneaza cate elemente sunt sub un pret dat
+        //O metoda care returneaza suma elementelor fara cel mai mic element
+        //O metoda care returneaza media celor mai mari trei elemente
+        //O metoda care returneaza lista primelor cinci elemente
+
+        //Project
+        //O aplicatie de stil stackoverflow - forum(useri, posts, comments, votes, etc
+    }
+}
---
+++ ExercitiiColectii2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace ExercitiiColectii2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Preturi preturiKaufland = new Preturi();
+
+            preturiKaufland.adaugarePret(33);
+            preturiKaufland.adaugarePret(25.40);
+
+            double pretCocaCola = 7.60;
+            preturiKaufland.adaugarePret(pretCocaCola);
+            Console.WriteLine($"Preturi Kaufland: {preturiKaufland}");
+            preturiKaufland.afisareListaPreturi();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(preturiKaufland.ToString());
+
+            Console.WriteLine("\n");
+
+            preturiKaufland.stergerePrimulElement();
+            preturiKaufland.stergereUltimulElement();
+            Console.WriteLine(preturiKaufland.ToString());
+
+            preturiKaufland.adaugarePret(pretCocaCola);
+            preturiKaufland.adaugarePret(pretCocaCola);
+            preturiKaufland.adaugarePret(28.99);
+            preturiKaufland.adaugarePret(35.25);
+            preturiKaufland.adaugarePret(41.02);
+
+            Console.WriteLine("\n");
+            preturiKaufland.sortareListaDePreturiCrescator();
+            Console.WriteLine(preturiKaufland.ToString());
+
+            preturiKaufland.sortareListaDePreturiDescrescator();
+            Console.WriteLine(preturiKaufland.ToString());
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(preturiKaufland.getElementAt(2));
+            Console.WriteLine(preturiKaufland.getElementAt(preturiKaufland.countPreturi() - 2));
+
+            Console.WriteLine("\n");
+
+            if (preturiKaufland.getElementAt(2) < preturiKaufland.getElementAt(preturiKaufland.countPreturi()-2))
+            {
+                Console.WriteLine(preturiKaufland.countPreturi() - 2);
+            }
+            else
+            {
+                Console.WriteLine(2);
+            }
+
+            List<double> rezultat = preturiKaufland.getListaPreturiAbove(26);
+            Console.WriteLine(rezultat.Count());
+            
+            foreach(double element in rezultat)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine(preturiKaufland.getNumberOfElementsBelow(41.02));
+            Console.WriteLine(preturiKaufland.sumWithoutMin());
+
+            List<double> rezultat2 = preturiKaufland.getListaPreturiAbove(7.6);
+            Console.WriteLine(rezultat2.Sum());
+
+            Console.WriteLine("\n");
+            Console.WriteLine(preturiKaufland.getNumberOfElementsBelow(41.02));
+            Console.WriteLine(preturiKaufland.ToString());
+
+            Console.WriteLine("\n");
+            Console.WriteLine(preturiKaufland.mediaCelorMaiMariTreiElemente());
+            Console.WriteLine(preturiKaufland.ToString());
+        }
+    }
+}
